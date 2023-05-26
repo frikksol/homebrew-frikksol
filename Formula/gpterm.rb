8d5b4668c594513650d55c249aeb6978c68dcb24
Frikk Herding <frikksol@gmail.com>
2023-05-27 00:26:14 +0200

    Added gpterm

---
+++ Formula/gpterm.rb
@@ -0,0 +1,11 @@
+class Gpterm < Formula
+  desc "chat-GPT from the comfort of your terminal. Written in rust of course"
+  homepage "https://github.com/frikksol/gpterm"
+  url "https://github.com/frikksol/gpterm/releases/download/v0.1.3/gpterm_v0.1.3_x86_64-apple-darwin.tar.gz"
+  sha256 "0e19db0c553587953413fc806bb27b906aeccbe53dfab33c65d632d6f22cd3d7"
+  version "0.1.2"
+
+  def install
+    bin.install "gpterm"
+  end
+end
